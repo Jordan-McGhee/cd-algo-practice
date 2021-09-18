9eecf0cb245269a62e5cd427aae046c9e2858157
Jordan McGhee <jordanmcghee95@gmail.com>
2021-09-18 13:14:05 -0400

    Added a couple more problems

--- strings/strings-todo-1.js
+++ strings/strings-todo-1.js
@@ -21,4 +21,76 @@ function countNonSpaces(str) {
     return joinedString.length
 }
 
-console.log(countNonSpaces("Honey pie, you are driving me crazy"))
+// console.log(countNonSpaces("Honey pie, you are driving me crazy"))
+
+
+// PARENS VALID
+// Valid sets of parentheses always open before they close, for example. For "Y(3(p)p(3)r)s", return true. Given "N(0(p)3", return false: not every parenthesis is closed. Given "N(0)t )0(k", return false, because the underlined ")" is premature: there is nothing open for it to close.
+
+function parensValid(str){
+    let count = 0
+
+    for (i=0; i < str.length; i++) {
+        if (str[i] == "(") {
+            count++
+        } else if (str[i] == ")") {
+            count--
+        }
+
+        if (count < 0) {
+            return false
+        }
+    }
+
+    if (count == 0) {
+        return true
+    }
+
+    return false
+}
+
+// console.log(parensValid("Y(3(p)p(3)r)s"))
+// console.log(parensValid("N(0)t )0(k"))
+// console.log(parensValid("((()))())(()"))
+// console.log(parensValid("(())()(())(((((())))))"))
+
+
+// BRACES VALID
+// Given a sequence of parentheses, braces and brackets, determine whether it is valid. Example:
+// "W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!" => true. "D(i{a}l[ t]o)n{e" => false. "A(1)s[O (n]0{t) 0}k" => false.
+
+function bracesValid(str) {
+    let parenCount = bracesCount = bracketsCount = 0
+
+    for (i=0; i < str.length; i++) {
+        if (str[i] == "(") {
+            parenCount++
+        } else if (str[i] == ")") {
+            parenCount--
+        } else if (str[i] == "[") {
+            bracesCount++
+        } else if (str[i] == "]") {
+            bracesCount--
+        } else if (str[i] == "{") {
+            bracketsCount++
+        } else if (str[i] == "}") {
+            bracketsCount--
+        }
+
+        if (parenCount < 0 || bracesCount < 0 || bracketsCount < 0) {
+            return false
+        }
+    }
+
+    if (parenCount == 0 && bracesCount == 0 && bracketsCount == 0) {
+        return true
+    }
+
+    return false
+}
+
+
+console.log(bracesValid("W(a{t}s[o(n{ c}o)m]e )h[e{r}e]!"))
+console.log(bracesValid("D(i{a}l[ t]o)n{e"))
+console.log(bracesValid("A(1)s[O (n]0{t) 0}k"))
+console.log(bracesValid("{([]})(())(){}{}[][][][]"))
